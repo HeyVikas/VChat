51e61186367eeed8301ad82d0d288a3494dbf79c
HeyVikas <namastelalwani@gmail.com>
2024-05-09 03:11:10 +0530

    Sign-in With google button

--- app/src/main/java/com/vikas/vchat/MainActivity.kt
+++ app/src/main/java/com/vikas/vchat/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.vikas.vchat.feature.login.LoginScreen
 import com.vikas.vchat.feature.splash.SplashScreen
 import com.vikas.vchat.ui.theme.VChatTheme
 
@@ -23,7 +24,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SplashScreen()
+//                    SplashScreen()
+                    LoginScreen()
                 }
             }
         }
